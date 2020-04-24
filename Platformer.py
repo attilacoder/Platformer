191d46df0b626e72f9386b2baaf2622a5308f26c
attilacoder <32287233+attilacoder@users.noreply.github.com>
2020-04-23 21:03:49 -0700

    Update Platformer.py

--- Platformer.py
+++ Platformer.py
@@ -17,14 +17,24 @@ w.fill(sky)
 right = False
 left = False
 
+def load_map(path):
+    f = open(path + '.txt','r')
+    data = f.read()
+    f.close()
+    data = data.split('\n')
+    game_map = []
+    for row in data:
+        game_map.append(list(row))
+    return game_map
+    
 p_pos = [50,50]
 x_speed = 4
-y_speed = 6
-y_acc = 0
-y_max = 6
+y_speed = 0
+j_speed = 6
+grav = 0.4
 jumping = False
 air_timer = 0
-
+space = 0
 p_rect = pygame.Rect(p_pos[0],p_pos[1],16,16)
 p_img = pygame.image.load('player.png')
 grass = pygame.image.load('grass.png')
@@ -32,16 +42,9 @@ dirt =  pygame.image.load('dirt.png')
 cloud = pygame.image.load('cloud.png')
 
 
-font = pygame.font.SysFont("Arial", 18)
 
-
-def update_fps():
-	fps = str(int(c.get_fps()))
-	fps_text = font.render(fps, 1, pygame.Color("coral"))
-	return fps_text
 playing = True
 while playing:
-    w.blit(update_fps(), (10,0))
     w.fill(sky)
     p_rect.x, p_rect.y = p_pos[0], p_pos[1]
 
@@ -56,32 +59,26 @@ while playing:
             if event.key == pygame.K_a:
                 left = True
             if event.key == pygame.K_SPACE:
-                y_acc = -6
+                space = 1
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_d:
                 right = False
             if event.key == pygame.K_a:
                 left = False
+            if event.key == pygame.K_SPACE:
+                space = 0
+
     if p_pos[1] > w_size[1]-p_img.get_height():
-        air_timer = 0
-        y_acc = 0
-    else:
-        airtimer += 1
-    p_pos[1] += y_acc
-    if jumping:
-        y_speed -= y_acc
-        if y_speed < -6 and y_speed > 0:
-            y_speed = 6
-            jumping = False
-
-
+        y_speed = space * -j_speed
+    elif y_speed < 10:
+        y_speed += grav
     if right:
         p_pos[0] += x_speed
     if left:
         p_pos[0] -= x_speed
 
 
-
+    p_pos[1] += y_speed
 
     w.blit(p_img, p_pos)
 
