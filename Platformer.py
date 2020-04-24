a27964650937b9c2c36c7236f382eec25e62784b
attilacoder <32287233+attilacoder@users.noreply.github.com>
2020-04-23 19:43:44 -0700

    Update Platformer.py

--- Platformer.py
+++ Platformer.py
@@ -19,11 +19,12 @@ left = False
 
 p_pos = [50,50]
 x_speed = 4
-y_speed = 0
-y_acc = 0.4
+y_speed = 6
+y_acc = 0
 y_max = 6
+jumping = False
 
-
+p_rect = pygame.Rect(p_pos[0],p_pos[1],16,16)
 p_img = pygame.image.load('player.png')
 grass = pygame.image.load('grass.png')
 dirt =  pygame.image.load('dirt.png')
@@ -32,7 +33,7 @@ cloud = pygame.image.load('cloud.png')
 playing = True
 while playing:
     w.fill(sky)
-
+    p_rect.x, p_rect.y = p_pos[0], p_pos[1]
 
 
     for event in pygame.event.get():
@@ -45,14 +46,22 @@ while playing:
             if event.key == pygame.K_a:
                 left = True
             if event.key == pygame.K_SPACE:
-                if y_speed > -y_acc:
-                    y_speed = y_acc
+                y_acc = -6
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_d:
                 right = False
             if event.key == pygame.K_a:
                 left = False
-
+    if p_pos[1] > w_size[1]-p_img.get_height():
+        y_acc = -y_acc
+    else:
+        y_acc += 0.2
+    p_pos[1] += y_acc
+    if jumping:
+        y_speed -= y_acc
+        if y_speed < -6 and y_speed > 0:
+            y_speed = 6
+            jumping = False
 
 
     if right:
@@ -62,8 +71,8 @@ while playing:
 
 
 
-    w.blit(p_img, p_pos)
 
+    w.blit(p_img, p_pos)
 
 
 
